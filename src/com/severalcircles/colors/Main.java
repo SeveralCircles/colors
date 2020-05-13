0213ad70c176cce702cb73ea0064291477c724fc
Aidan Veney <aidanveney@gmail.com>
2020-05-13 12:45:15 -0400

    Added about command

--- src/com/severalcircles/colors/Main.java
+++ src/com/severalcircles/colors/Main.java
@@ -1,16 +1,18 @@
 package com.severalcircles.colors;
 
+import com.severalcircles.colors.commands.Command_about;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-
+        System.out.println("Enabling commands...");
+        this.getCommand("about").setExecutor(new Command_about());
     }
 
     @Override
     public void onDisable() {
-
+        System.out.println("See ya later!");
     }
 }
 //Look, it's from Bowser
---
+++ src/com/severalcircles/colors/commands/Command_about.java
@@ -0,0 +1,18 @@
+package com.severalcircles.colors.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Command_about implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
+        // Casts the instance of commandSender to an instance of Player to give us more options
+        Player player = (Player) commandSender;
+        player.sendMessage(ChatColor.GOLD + "Colors\n" + ChatColor.DARK_RED + "Version: Red 1\n" + ChatColor.GREEN + "By Aidan, Mo and Tyler, May 2020");
+        return true;
+    }
+}
