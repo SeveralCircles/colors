8cb4def428be20825b5ed2117cbd63ae03986511
Aidan Veney <aidanveney@gmail.com>
2020-05-14 11:53:18 -0400

    Added configuration support and default configuration file

--- src/com/severalcircles/colors/Main.java
+++ src/com/severalcircles/colors/Main.java
@@ -4,9 +4,24 @@ import com.severalcircles.colors.commands.CommandAbout;
 import com.severalcircles.colors.commands.CommandPersona;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
+        try {
+            System.out.println("Build number: " + classBuildTimeMillis());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Setting up config...");
+        this.saveDefaultConfig();
+        if (!this.getConfig().getBoolean("enablePlugin")) {
+            System.out.println("Plugin disabled in config. Disabling.");
+            getServer().getPluginManager().disablePlugin(this);
+        }
         System.out.println("Enabling commands...");
         this.getCommand("about").setExecutor(new CommandAbout());
         this.getCommand("persona").setExecutor(new CommandPersona());
@@ -16,8 +31,32 @@ public class Main extends JavaPlugin {
     public void onDisable() {
         System.out.println("See ya later!");
     }
-}
-//Look, it's from Bowser
+
+    //Look, it's from Bowser
 //It's me, Jerry Seinfeild from Garfeild Kart
 //Joker Persona 5
 // Testing codacy
+    private long classBuildTimeMillis() throws URISyntaxException, IllegalStateException, IllegalArgumentException {
+        URL resource = getClass().getResource(getClass().getSimpleName() + ".class");
+        if (resource == null) {
+            throw new IllegalStateException("Failed to find class file for class: " +
+                    getClass().getName());
+        }
+
+        if (resource.getProtocol().equals("file")) {
+
+            return new File(resource.toURI()).lastModified();
+
+        } else if (resource.getProtocol().equals("jar")) {
+
+            String path = resource.getPath();
+            return new File(path.substring(5, path.indexOf("!"))).lastModified();
+
+        } else {
+
+            throw new IllegalArgumentException("Unhandled url protocol: " +
+                    resource.getProtocol() + " for class: " +
+                    getClass().getName() + " resource: " + resource.toString());
+        }
+    }
+}
