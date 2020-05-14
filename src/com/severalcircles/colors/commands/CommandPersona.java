c4620f07f9c313296dc1a828ba3339769021be5a
Aidan Veney <aidanveney@gmail.com>
2020-05-14 12:32:47 -0400

    Cleaned up unused imports in CommandPersona

--- src/com/severalcircles/colors/commands/CommandPersona.java
+++ src/com/severalcircles/colors/commands/CommandPersona.java
@@ -5,8 +5,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
 public class CommandPersona implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
