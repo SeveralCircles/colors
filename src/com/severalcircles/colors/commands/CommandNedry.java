3d32915091470b8482f6b926dc715b77c7846fbb
Aidan Veney <aidanveney@gmail.com>
2020-05-15 10:49:45 -0400

    Added permissions

--- src/com/severalcircles/colors/commands/CommandNedry.java
+++ src/com/severalcircles/colors/commands/CommandNedry.java
@@ -10,12 +10,15 @@ import org.bukkit.entity.Player;
 public class CommandNedry implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
-        Player player= (Player) commandSender;
+        Player player = (Player) commandSender;
+        if (!player.hasPermission("com.severalcircles.colors.magicword")) {
+            player.sendMessage(ChatColor.YELLOW + "Ah Ah Ah! You didn't say the magic word!");
+        }
         player.getWorld().spawnEntity(player.getLocation(), EntityType.PRIMED_TNT);
         player.getWorld().spawnEntity(player.getLocation(), EntityType.PRIMED_TNT);
         player.getWorld().spawnEntity(player.getLocation(), EntityType.PRIMED_TNT);
         int chatspam = 0;
-        while (chatspam<3000) {
+        while (chatspam < 3000) {
             player.sendMessage(ChatColor.YELLOW + "Ah Ah Ah! You didn't say the magic word!");
             chatspam++;
         }
