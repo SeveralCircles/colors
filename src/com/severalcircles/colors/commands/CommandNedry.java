131119d714fd14f3cc53519426ac17f766df378d
Daddy Thanos <mbrackenrich23@biomedscienceacademy.org>
2020-05-15 10:24:03 -0400

    Added feature to CommandNedry that spawns 3 PRIMED_TNT

--- src/com/severalcircles/colors/commands/CommandNedry.java
+++ src/com/severalcircles/colors/commands/CommandNedry.java
@@ -4,12 +4,16 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
 public class CommandNedry implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         Player player= (Player) commandSender;
+        player.getWorld().spawnEntity(player.getLocation(), EntityType.PRIMED_TNT);
+        player.getWorld().spawnEntity(player.getLocation(), EntityType.PRIMED_TNT);
+        player.getWorld().spawnEntity(player.getLocation(), EntityType.PRIMED_TNT);
         int chatspam = 0;
         while (chatspam<3000) {
             player.sendMessage(ChatColor.YELLOW + "Ah Ah Ah! You didn't say the magic word!");
