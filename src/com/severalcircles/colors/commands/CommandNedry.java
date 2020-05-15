4cc15bdfd85e53b74d67d691c1848e4e7daef700
Daddy Thanos <mbrackenrich23@biomedscienceacademy.org>
2020-05-15 10:57:45 -0400

    Added Player target = Bukkit.getPlayer(args[0]); to CommandNedry

--- src/com/severalcircles/colors/commands/CommandNedry.java
+++ src/com/severalcircles/colors/commands/CommandNedry.java
@@ -1,5 +1,6 @@
 package com.severalcircles.colors.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,10 +12,11 @@ public class CommandNedry implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         Player player = (Player) commandSender;
+        Player target = Bukkit.getPlayer(args[0]);
         if (!player.hasPermission("com.severalcircles.colors.magicword")) {
             player.sendMessage(ChatColor.YELLOW + "Ah Ah Ah! You didn't say the magic word!");
         }
-        player.getWorld().spawnEntity(player.getLocation(), EntityType.PRIMED_TNT);
+        player.getWorld().spawnEntity(player.getLocation(), EntityType.);
         player.getWorld().spawnEntity(player.getLocation(), EntityType.PRIMED_TNT);
         player.getWorld().spawnEntity(player.getLocation(), EntityType.PRIMED_TNT);
         int chatspam = 0;
