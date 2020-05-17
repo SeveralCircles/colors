1abd70a6380ddf33dad40a227f11c91292b22a22
Aidan Veney <aidanveney@gmail.com>
2020-05-17 19:16:51 -0400

    Added player information file

--- src/com/severalcircles/colors/Main.java
+++ src/com/severalcircles/colors/Main.java
@@ -3,16 +3,27 @@ package com.severalcircles.colors;
 import com.severalcircles.colors.commands.CommandAbout;
 import com.severalcircles.colors.commands.CommandNedry;
 import com.severalcircles.colors.commands.CommandPersona;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 
 public class Main extends JavaPlugin {
+    public static Main plugin;
     public static int random;
+    private File playerConfigFile;
+    private FileConfiguration playerConfig;
+
     @Override
     public void onEnable() {
+        plugin = this;
         try {
             System.out.println("Build number: " + classBuildTimeMillis());
         } catch (URISyntaxException e) {
@@ -20,6 +31,16 @@ public class Main extends JavaPlugin {
         }
         System.out.println("Setting up config...");
         this.saveDefaultConfig();
+        createPlayerConfig();
+        Player[] onlinePlayers = (Player[]) Bukkit.getOnlinePlayers().toArray();
+        for (int i = onlinePlayers.length; i > -1; i--) {
+            if (playerConfig.get(onlinePlayers[i].getName() + ".iv").equals(null)) {
+                System.out.println("Player " + onlinePlayers[i].getName() + " does not have unique individual values assigned. Assigning now.");
+                // Generate unique individual values
+                playerConfig.createSection(onlinePlayers[i].getName() + ".iv");
+                playerConfig.set(onlinePlayers[i].getName() + ".iv", (int) Math.round(Math.random() * 100000));
+            }
+        }
         System.out.println("Loading config...");
         random = this.getConfig().getInt("random");
         if (!this.getConfig().getBoolean("enablePlugin")) {
@@ -62,6 +83,21 @@ public class Main extends JavaPlugin {
             throw new IllegalArgumentException("Unhandled url protocol: " +
                     resource.getProtocol() + " for class: " +
                     getClass().getName() + " resource: " + resource.toString());
+        }
+    }
+
+    private void createPlayerConfig() {
+        playerConfigFile = new File(getDataFolder(), "players.yml");
+        if (!playerConfigFile.exists()) {
+            playerConfigFile.getParentFile().mkdirs();
+            saveResource("players.yml", false);
+        }
+
+        playerConfig = new YamlConfiguration();
+        try {
+            playerConfig.load(playerConfigFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
         }
     }
 }
