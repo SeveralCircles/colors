c9cc64cc919f855d0f8307efac524da448099298
Tyler Williard <twilliard23@biomedscienceacademy.org>
2020-05-18 10:18:07 -0400

    Made it so the Sans command now allows you to target someone.

--- src/com/severalcircles/colors/commands/CommandSans.java
+++ src/com/severalcircles/colors/commands/CommandSans.java
@@ -1,5 +1,7 @@
 package com.severalcircles.colors.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,7 +12,11 @@ public class CommandSans implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         Player player = (Player) commandSender;
+        Player target = Bukkit.getPlayer(args[0]);
+        Bukkit.broadcastMessage(ChatColor.BLUE + "getttttttttttt dunked on!");
         player.getWorld().spawnEntity(player.getLocation(), EntityType.SKELETON);
         return false;
     }
 }
+
+//sans is about to jump you in a dark alley way if you're not careful :flushed:
