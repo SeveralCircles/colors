0c900f5b81eaed0b7bb727536e212f4cadfb4f67
Aidan Veney <aidanveney@gmail.com>
2020-05-27 10:01:42 -0400

    Registered the Chat Event in the Main class

--- src/com/severalcircles/colors/system/Main.java
+++ src/com/severalcircles/colors/system/Main.java
@@ -4,6 +4,7 @@ import com.severalcircles.colors.commands.CommandAbout;
 import com.severalcircles.colors.commands.CommandNedry;
 import com.severalcircles.colors.commands.CommandPersona;
 import com.severalcircles.colors.commands.CommandSans;
+import com.severalcircles.colors.events.ChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -32,6 +33,7 @@ public class Main extends JavaPlugin {
         this.getCommand("persona").setExecutor(new CommandPersona());
         this.getCommand("sans").setExecutor(new CommandSans());
         this.getCommand("access").setExecutor(new CommandNedry());
+        this.getPluginLoader().createRegisteredListeners(new ChatEvent(), this);
     }
 
     @Override
