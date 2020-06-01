e5136ff0340410e26076295fef27e9dcf60d8029
Aidan Veney <aidanveney@gmail.com>
2020-06-01 11:21:21 -0400

    Fixed switch statement

--- src/com/severalcircles/colors/commands/CommandUnknown.java
+++ src/com/severalcircles/colors/commands/CommandUnknown.java
@@ -1,6 +1,5 @@
 package com.severalcircles.colors.commands;
 
-import com.severalcircles.colors.system.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -22,55 +21,75 @@ public class CommandUnknown implements CommandExecutor {
             case 0:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 120, 1));
                 player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 300, 1));
+                break;
             case 1:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 120, 1));
+                break;
             case 2:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 120, 1));
+                break;
             case 3:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 120, 2));
+                break;
             case 4:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 120, 1));
+                break;
             case 5:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 180, 1));
+                break;
             case 6:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 120, 1));
+                break;
             case 7:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 120, 50));
+                break;
             case 8:
                 player.getWorld().spawnEntity(player.getLocation(), EntityType.PRIMED_TNT);
                 player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 5, 100));
+                break;
             case 9:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.HERO_OF_THE_VILLAGE, 120, 1));
+                break;
             case 10:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 120, 1));
+                break;
             case 11:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 120, 1));
+                break;
             case 12:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 20, 1));
+                break;
             case 13:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 120, 1));
                 player.getWorld().spawnEntity(player.getLocation(), EntityType.BAT);
+                break;
             case 14:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 120, 1));
+                break;
             case 15:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.WATER_BREATHING, 120, 1));
                 player.addPotionEffect(new PotionEffect(PotionEffectType.DOLPHINS_GRACE, 120, 1));
+                break;
             case 16:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, 120, 10));
+                break;
             case 17:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.HUNGER, 15, 10));
+                break;
             case 18:
                 player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 30, 100));
                 Bukkit.broadcastMessage(ChatColor.DARK_RED + "ACTIVATE INSTANT KILL MODE");
+                break;
             case 19:
                 for (int i = 0; i < 420; i++) {
                     player.getWorld().spawnEntity(player.getLocation(), EntityType.EXPERIENCE_ORB);
                 }
                 player.getWorld().spawnEntity(player.getLocation(), EntityType.PIG);
                 player.sendMessage(ChatColor.YELLOW + "You've been greeted by the good luck pig");
+                break;
             case 20:
                 player.getWorld().spawnEntity(player.getLocation(), EntityType.PLAYER);
-
+                break;
 
         }
         return true;
